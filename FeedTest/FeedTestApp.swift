b24bbdc8fe624d5964fa818d0e4d91cd09128068
Yehor Sobko <yehorsobko@Yehors-MacBook-Pro.local>
2024-05-12 16:35:05 +0200

    initial commit

---
+++ FeedTest/FeedTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedTestApp.swift
+//  FeedTest
+//
+//  Created by Yehor Sobko on 12/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FeedTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
